3c590e9c889aa52b34e15bcc6355de9d5a68afb9
Tatiana <tatiana.joli@outlook.com>
2024-05-06 11:38:34 -0400

    item description moved from item to auction

--- schema.sql
+++ schema.sql
@@ -78,6 +78,115 @@ LOCK TABLES `end_user` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `inlcuded_item`
+--
+
+DROP TABLE IF EXISTS `inlcuded_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inlcuded_item` (
+  `item_id` int NOT NULL,
+  `auction_id` int NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `auction_id` (`auction_id`),
+  CONSTRAINT `inlcuded_item_ibfk_1` FOREIGN KEY (`auction_id`) REFERENCES `posted_auction` (`auction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inlcuded_item`
+--
+
+LOCK TABLES `inlcuded_item` WRITE;
+/*!40000 ALTER TABLE `inlcuded_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inlcuded_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_alert_set`
+--
+CREATE DATABASE IF NOT EXISTS `cs336project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cs336project`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cs336project
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bids_on`
+--
+
+DROP TABLE IF EXISTS `bids_on`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bids_on` (
+  `bid_id` int NOT NULL,
+  `auction_id` int DEFAULT NULL,
+  `bidder_id` int DEFAULT NULL,
+  `upper_limit` float DEFAULT NULL,
+  `is_Highest` tinyint(1) DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `when_placed` datetime DEFAULT NULL,
+  PRIMARY KEY (`bid_id`),
+  KEY `auction_id` (`auction_id`),
+  KEY `bidder_id` (`bidder_id`),
+  CONSTRAINT `bids_on_ibfk_1` FOREIGN KEY (`auction_id`) REFERENCES `posted_auction` (`auction_id`),
+  CONSTRAINT `bids_on_ibfk_2` FOREIGN KEY (`bidder_id`) REFERENCES `end_user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bids_on`
+--
+
+LOCK TABLES `bids_on` WRITE;
+/*!40000 ALTER TABLE `bids_on` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bids_on` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `end_user`
+--
+
+DROP TABLE IF EXISTS `end_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `end_user` (
+  `user_id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(5) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` varchar(15) DEFAULT NULL,
+  `prev_auctions` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `end_user`
+--
+
+LOCK TABLES `end_user` WRITE;
+/*!40000 ALTER TABLE `end_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `end_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `included_item`
 --
 
@@ -85,9 +194,8 @@ DROP TABLE IF EXISTS `included_item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `included_item` (
-  `item_id` int NOT NULL,
+  `item_id` int NOT NULL AUTO_INCREMENT,
   `auction_id` int NOT NULL,
-  `description` varchar(250) DEFAULT NULL,
   `category` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`item_id`),
   KEY `auction_id` (`auction_id`),
@@ -141,13 +249,13 @@ DROP TABLE IF EXISTS `posted_auction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `posted_auction` (
-  `auction_id` int NOT NULL,
+  `auction_id` int NOT NULL AUTO_INCREMENT,
   `seller_id` int NOT NULL,
   `when_closes` datetime DEFAULT NULL,
-  `history_of_bids` varchar(500) DEFAULT NULL,
-  `status` char(6) DEFAULT 'Open',
+  `description` varchar(250) DEFAULT NULL, -- Updated to serve as the description for both auction and item
+  `status` varchar(50) DEFAULT NULL,
   `bid_increment` int DEFAULT NULL,
-  `imageCount` int DEFAULT 0,
+  `imageCount` int DEFAULT NULL,
   PRIMARY KEY (`auction_id`),
   KEY `seller_id` (`seller_id`),
   CONSTRAINT `posted_auction_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `end_user` (`user_id`)
